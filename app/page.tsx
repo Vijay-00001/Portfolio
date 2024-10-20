c391ef8e0b04a80e57d7913121479fdcb34ddbd7
Vijay <vijay.active.001@gmail.com>
2024-10-20 12:06:38 +0530

    Ticket 003

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/main/Hero';
+import Skills from '@/components/main/Skills';
 
 export default function Home() {
    return (
@@ -6,6 +7,7 @@ export default function Home() {
          <div className="flex flex-col h-[850px] gap-20">
             <Hero />
          </div>
+         <Skills />
       </main>
    );
 }
