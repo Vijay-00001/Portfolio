a31c97e41af941c77bdf54b1f849e97e966b3fb5
Vijay <vijay.active.001@gmail.com>
2024-10-20 10:31:15 +0530

    Ticket 002

---
+++ components/main/Hero.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import HeroContent from '../sub/HeroContent';
+
+const Hero = () => {
+   return (
+      <div className="relative flex flex-col w-full h-full">
+         <video
+            autoPlay
+            loop
+            muted
+            className="rotate-180 absolute top-[-340px] w-full h-full left-0 z-[1] object-cover"
+         >
+            <source src="/public_blackhole.webm" type="video/webm" />
+         </video>
+         <HeroContent />
+      </div>
+   );
+};
+
+export default React.memo(Hero);
